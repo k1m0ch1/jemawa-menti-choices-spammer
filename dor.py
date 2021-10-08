354f46a72d60fbcf7f90ff4f36ff1e5819def41f
k1m0ch1 <yahya.kimochi@gmail.com>
2021-10-08 12:31:11 +0700

    change: remove .payload and change the new method to input data, also add for ranking

--- dor.py
+++ dor.py
@@ -18,9 +18,12 @@ if len(sys.argv) < 2:
 
 TARGET = sys.argv[1]
 KEY = TARGET.split('/',)[3]
+DATA_PAGE = f"https://www.menti.com/core/vote-keys/{KEY}/series"
 
-firstPage = requests.get(TARGET, headers={
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+
+firstPage = requests.get(DATA_PAGE, headers={
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
 })
 
 if firstPage.status_code != 200:
@@ -28,28 +31,25 @@ if firstPage.status_code != 200:
     print(f"{firstPage.text}")
     sys.exit(1)
 
-soup = bs(firstPage.text, 'html.parser')
-script = soup.find_all('script')[5]
-p = re.compile('window.__CONFIG__ = (.*);').search(script.string)
-CONFIG = json.loads(p.group(1))
-
-p = re.compile('window.__INITIAL_STATE__ = (.*);').search(script.string)
-INIT = json.loads(p.group(1))
-
-PRESENTER_ID = INIT['pace']['presenter']['presenterId']
+INIT = firstPage.json()
+PRESENTER_ID = INIT['pace']['active']
 PRESENTER_QUESTION = INIT['questions'][0] # default
 for question in INIT['questions']:
     if PRESENTER_ID == question['id']:
-        if question['type'] != 'choices':
-            print(f"WTF this is {question['type']}, didn't I tell you this is only for MULTIPLE CHOICE? (＃`Д´)")
-            print("just wait until the presenter show the multiple choices to vote")
-            sys.exit(1)
+        # if question['type'] not in ['choices', 'ranking']:
+        #     print(f"WTF this is {question['type']}, didn't I tell you this is only for MULTIPLE CHOICE? (＃`Д´)")
+        #     print("just wait until the presenter show the multiple choices to vote")
+        #     sys.exit(1)
         PRESENTER_QUESTION = question
         break
-pqi = { f"{item['id']}": item['label'] for item in PRESENTER_QUESTION['choices']}
-CURRENT_ID = INIT['pace']['presenter']['presenterId']
 
-print(f"Current Presenter Page {PRESENTER_ID} {PRESENTER_QUESTION['question']}\n")
+
+pqi = { f"{item['id']}": item['label'] for item in PRESENTER_QUESTION['choices']}
+CURRENT_ID = INIT['pace']['active']
+QUESTIONS = {question['id']: question for question in INIT['questions']}
+
+print(f"Current Presenter Page {QUESTIONS[PRESENTER_ID]['type']} {PRESENTER_ID} {PRESENTER_QUESTION['question']}\n")
+
 for choice in PRESENTER_QUESTION['choices']:
     print(f"ID {choice['id']} LABEL {choice['label']}")
 choice = input(f"\nWhich ID you want to vote: ")
@@ -63,37 +63,38 @@ if sure == 'n':
 bar = Bar('w00t w00t', max=int(loop))
 for until in range(0, int(loop)):
 
-    getIdentifier = requests.post("https://www.menti.com/core/identifiers", json={}, headers={
+    headers = {
         "origin": "https://menti.com",
         "referer": TARGET,
         "accept": "application/json",
         "content-type": "application/json; charset=UTF-8",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
-    })
+    }
+
+    getIdentifier = requests.post("https://www.menti.com/core/identifiers", json={}, headers=headers)
 
     if getIdentifier.status_code != 200:
         print(f"{getIdentifier.status_code} yo I can't get the Identifier (⊙_⊙)")
         print(getIdentifier.text)
         break
 
-    IDENTIFIER = getIdentifier.json()['identifier']
-
-    vote = requests.post(f"https://www.menti.com/core/votes/{PRESENTER_ID}", headers={
-        "origin": "https://menti.com",
-        "referer": TARGET,
-        "accept": "application/json",
-        "content-type": "application/json; charset=UTF-8",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
-        "x-identifier": IDENTIFIER
-    }, json={
-        'question_type': 'choices',
+    headers['x-identifier'] = getIdentifier.json()['identifier']
+    
+    DATA = {
+        'question_type': QUESTIONS[PRESENTER_ID]['type'],
         'vote': choice
-    })
+    }    
+
+    if QUESTIONS[PRESENTER_ID]['type'] == "ranking":
+        DATA['vote'] = [int(choice)]
+
+    vote = requests.post(f"https://www.menti.com/core/votes/{PRESENTER_ID}", headers=headers, json=DATA)
 
     if vote.status_code != 200:
         print(f"{vote.status_code} HAHAHAHAHA LOOKS LIKE ERROR, LOOKS WHAT YOU DID ┐('～`;)┌")
         print(vote.text)
         break
+    
     bar.next()
 bar.finish()
 
